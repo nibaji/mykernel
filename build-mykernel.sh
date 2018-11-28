be5b7db9acd219050ba43b72d8dc20117fda4c43
Nidhun Balaji T R <nidhunbalaji@gmail.com>
2018-11-28 18:02:52 +0530

    script: zip: fix zip extention Replace dots in zip filename's variable with underscores Dot char in filename breaks zip file extention
    Signed-off-by: Nidhun Balaji T R <nidhunbalaji@gmail.com>

--- build-mykernel.sh
+++ build-mykernel.sh
@@ -853,7 +853,11 @@ if [ -f  "$out_dir/arch/arm64/boot/Image.gz-dtb" ]
     echo -e "$r1 ******** $o"
     echo -e "$g zipping.. $o"
     echo -e "$r1 ******** $o"
-    tc=$(echo $tc | sed 's/\./_/g') #dot char breaks the zip filename
+    #dot char breaks the zip filename extension
+    krnl_name=$(echo $krnl_name | sed 's/\./_/g')
+    tc=$(echo $tc | sed 's/\./_/g')
+    device_name=$(echo $device_name | sed 's/\./_/g')
+    krnl_rev=$(echo $krnl_rev | sed 's/\./_/g')
     zip -r9 $krnl_name-$tc-$device_name-$krnl_rev-$built_time *
     echo -e "$r1 **************************** $o"
     echo -e "$g $krnl_name - $krnl_rev is ready $o"
